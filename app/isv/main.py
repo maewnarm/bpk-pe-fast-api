3ca5299d34dc69b6ea0fb5ec9b0757621a7c7afb
voramet <vorametz@gmail.com>
2022-03-18 16:16:03 +0700

    20220318 add websocket

--- app/isv/main.py
+++ app/isv/main.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,UploadFile,File
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from typing import Optional
+import shutil
 
 from . import crud, schemas
 from ..dependencies import get_db
@@ -23,3 +25,13 @@ async def get_records(param: schemas.IsvRead = Depends(), db: Session = Depends(
 @router.post("/records/")
 async def create_record(record: schemas.IsvCreate, db: Session = Depends(get_db)):
     return crud.create_record(db, record)
+
+
+@router.post("/upload")
+async def upload(file: UploadFile=File(...)):
+    with open(f'D:\\IoT\\Web Applications\\upload\\{file.filename}', 'wb') as buffer:
+        content = await file.read()
+        buffer.write(content)
+        buffer.close()
+        # shutil.copyfileobj(file.file,buffer)
+    return JSONResponse(content={"filename": file.filename},status_code=200)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
+from os import environ
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import fastapi_mail
 
-from . import isv
+from .fastapi_socketio import SocketManager,createSocket
+from . import isv,fastapi_email
 from .database import engine
+from .fastapi_email import *
 
 from .isv import models, main
 
@@ -10,18 +14,19 @@ isv.models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "http://localhost",
-    "dnth.bpk-pe.apps"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 app.include_router(isv.main.router)
+app.include_router(fastapi_email.email.router)
+
+# socket io
+sio = SocketManager(app=app)
+createSocket(sio=sio)
